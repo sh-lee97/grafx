9eedfeddfda1a8a45b2b496d637ba520a236aa6e
sh-lee <sh-lee97@gmail.com>
2024-08-19 03:15:11 +0000

    misdie_to_mono ... mimo handling check

--- src/grafx/processors/stereo.py
+++ src/grafx/processors/stereo.py
@@ -167,3 +167,40 @@ class StereoToMidSide(nn.Module):
             :python:`Dict[str, Tuple[int, ...]]`: A dictionary that contains each parameter tensor's shape.
         """
         return {}
+
+
+class MidSideToStereo(nn.Module):
+    r"""
+    A simple mid-side-to-stereo conversion.
+    """
+
+    def __init__(self, normalize=True):
+        super().__init__()
+        self.normalization_const = INV_SQRT_2 if normalize else 0.5
+
+    def forward(self, mid, side):
+        r"""
+        Processes input audio with the processor and given parameters.
+
+        Args:
+            mid (:python:`FloatTensor`, :math:`B \times 1 \times L`):
+                A batch of mid audio signals.
+            side (:python:`FloatTensor`, :math:`B \times 1 \times L`):
+                A batch of side audio signals.
+
+        Returns:
+            :python:`FloatTensor`: A batch of output signals of shape :math:`B \times 2 \times L`.
+        """
+        b, c, t = mid.shape
+        assert c == 1
+        left, right = mid + side, mid - side
+        output_signals = torch.cat([left, right], 1)
+        output_signals = output_signals * self.normalization_const
+        return output_signals
+
+    def parameter_size(self):
+        """
+        Returns:
+            :python:`Dict[str, Tuple[int, ...]]`: A dictionary that contains each parameter tensor's shape.
+        """
+        return {}
