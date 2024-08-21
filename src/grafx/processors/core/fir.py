a3ff37f8990594c8d9ee719cfe427bb4e82fab0d
sh-lee <sh-lee97@gmail.com>
2024-08-21 12:44:09 +0000

    fir (refactor required)

--- src/grafx/processors/core/fir.py
+++ src/grafx/processors/core/fir.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from grafx.processors.core.fft_filterbank import fft_triangular_filterbank
+from grafx.processors.core.fft_filterbank import TriangularFilterBank
 
 
 def get_window(window_type, window_length, **kwargs):
@@ -16,6 +16,8 @@ def get_window(window_type, window_length, **kwargs):
             return torch.bartlett_window(window_length, **kwargs)
         case "kaiser":
             return torch.kaiser_window(window_length, **kwargs)
+        case "rectangular" | "none" | "boxcar" | None:
+            return None
         case _:
             raise ValueError(f"Unsupported window type: {window_type}")
 
@@ -29,28 +31,6 @@ def log_magnitude_to_zerophase_fir(
     shape, f = shape[:-1], shape[-1]
     log_magnitude = log_magnitude.view(-1, f)
     magnitude = torch.exp(log_magnitude)
-    ir = torch.fft.irfft(magnitude, n=fir_len)
-    shifts = fir_len // 2
-    ir = torch.roll(ir, shifts=shifts, dims=-1)
-    if window is not None:
-        ir = ir * window[None, :]
-    ir = ir.view(*shape, -1)
-    return ir
-
-
-def log_energy_to_zerophase_fir_with_filterbank(
-    log_energy,
-    fir_len,
-    filterbank,
-    window=None,
-    eps=1e-7,
-):
-    shape = log_energy.shape
-    shape, f = shape[:-1], shape[-1]
-    log_energy = log_energy.view(-1, f)
-    energy = torch.exp(log_energy)
-    magnitude = torch.matmul(filterbank, energy)
-    magnitude = torch.sqrt(magnitude + eps)
     ir = torch.fft.irfft(magnitude, n=fir_len)
     shifts = fir_len // 2
     ir = torch.roll(ir, shifts=shifts, dims=-1)
@@ -96,54 +76,48 @@ class ZeroPhaseFIR(nn.Module):
 class ZeroPhaseFilterBankFIR(nn.Module):
     def __init__(
         self,
-        num_energy_bins=1024,
-        scale="bark_traunmuller",
-        n_filters=80,
-        f_min=40,
-        f_max=None,
-        sr=None,
-        eps=1e-7,
+        num_frequency_bins=1024,
+        use_filterbank=False,
+        filterbank_kwargs={},
         window="hann",
-        **window_kwargs,
+        window_kwargs={},
+        eps=1e-7,
     ):
         super().__init__()
 
-        assert (
-            f_max is not None or sr is not None
-        ), "Either f_max or sr must be provided."
-
-        self.num_energy_bins = num_energy_bins
-        self.fir_len = 2 * num_energy_bins - 1
+        self.num_frequency_bins = num_frequency_bins
+        self.fir_len = 2 * num_frequency_bins - 1
         self.eps = eps
 
-        filterbank = fft_triangular_filterbank(
-            n_freqs=num_energy_bins,
-            f_min=f_min,
-            f_max=f_max,
-            n_filters=n_filters,
-            scale=scale,
-            attach_remaining=True,
-        )
-        filterbank = filterbank.T
-        self.register_buffer("filterbank", filterbank)
+        self.use_filterbank = use_filterbank
+        if self.use_filterbank:
+            self.filterbank = TriangularFilterBank(
+                num_frequency_bins=num_frequency_bins, **filterbank_kwargs
+            )
 
         if isinstance(window, torch.Tensor):
             self.register_buffer("window", window)
         else:
-            match window:
-                case "rectangular" | "none" | "boxcar" | None:
-                    self.window = None
-                case _:
-                    window = get_window(
-                        window_type=window, fir_len=self.fir_len, **window_kwargs
-                    )
-                    self.register_buffer("window", window)
+            window = get_window(
+                window_type=window, window_length=self.fir_len, **window_kwargs
+            )
+            self.register_buffer("window", window)
 
-    def forward(self, filterbank_log_energy):
-        return log_energy_to_zerophase_fir_with_filterbank(
-            filterbank_log_energy,
-            fir_len=self.fir_len,
-            window=self.window,
-            filterbank=self.filterbank,
-            eps=self.eps,
-        )
+    def forward(self, log_magnitude):
+        shape = log_magnitude.shape
+        shape, f = shape[:-1], shape[-1]
+        log_magnitude = log_magnitude.view(-1, f)
+
+        magnitude = torch.exp(log_magnitude)
+        if self.use_filterbank:
+            energy = magnitude.square()
+            energy = self.filterbank(energy)
+            magnitude = torch.sqrt(energy + self.eps)
+        ir = torch.fft.irfft(magnitude, n=self.fir_len)
+        shifts = self.fir_len // 2
+        ir = torch.roll(ir, shifts=shifts, dims=-1)
+        if self.window is not None:
+            ir = ir * self.window[None, :]
+
+        ir = ir.view(*shape, -1)
+        return ir
